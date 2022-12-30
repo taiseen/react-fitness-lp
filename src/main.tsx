2d0dbe61ad557bc5e22b9cec6b7ef98ecad020a7
taiseen <taiseen.cse@gmail.com>
2022-12-30 16:58:28 +0600

    chore: css style change & template remove

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
 import App from './App'
-import './index.css'
+import './styles/index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
